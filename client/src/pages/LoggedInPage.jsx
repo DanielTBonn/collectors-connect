77665db8a51772f9e80292e528dda2079e312804
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-11-17 08:27:50 -0600

    Added better css for feed cards

--- client/src/pages/LoggedInPage.jsx
+++ client/src/pages/LoggedInPage.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@apollo/client";
 import { GET_ME, GET_USER_COLLECTIONS } from "../utils/queries";
 import UserProfile from "../components/UserProfile";
-import Cards from "../components/Cards";
+import FeedCards from "../components/FeedCards";
 
 const LoggedInPage = () => {
   const { loading: userLoading, data: userData } = useQuery(GET_ME);
@@ -18,11 +18,11 @@ const LoggedInPage = () => {
         Hello {user.username}
       </div>
       <UserProfile userProfile={user} />
-      <div className="collections-container">
+      <div className="collections-container profile-collections">
         {collectionsLoading ? (
           <p>Loading collections...</p>
         ) : (
-          <Cards collections={userCollections} />
+          <FeedCards collections={userCollections} />
         )}
       </div>
     </>
