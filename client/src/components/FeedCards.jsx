6fd5eb232f1f745f8b9319fcbcc1cca5000d9718
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-11-16 19:56:14 -0600

    Added another example to FeedCards

--- client/src/components/FeedCards.jsx
+++ client/src/components/FeedCards.jsx
@@ -10,31 +10,33 @@ function Cards() {
           <ul className="cards__items feed-ul">
             <CardItem
               src="https://www.moraswines.com/wp-content/uploads/2023/05/Guadalupe.jpeg"
-              text="Here is my collection of rare bottles of rare Texas bourbon"
+              text="Come see my collection of rare bottles of Texas bourbon"
               label="Texas Bourbon"
               path="/texasBourbon"
-              className="feed-li"
             />
             <CardItem
               src="https://obviousshirts.com/cdn/shop/products/autographed-baseball-card-signed-by-thomas-ian-nicholas-463981.png?v=1687616953&width=1080"
-              text="Here is my collection of rare baseball cards."
+              text="Come see my collection of rare baseball cards."
               label="Baseball Cards"
               path="/baseballCards"
-              className="feed-li"
             />
             <CardItem
               src="https://i.etsystatic.com/25352708/r/il/25032d/3631422808/il_fullxfull.3631422808_fq5q.jpg"
-              text="Here is my collection of rare anime pins."
+              text="Come see my collection of rare anime pins."
               label="Anime Pins"
               path="/animePins"
-              className="feed-li"
             />
             <CardItem
               src="https://cdn-cahdg.nitrocdn.com/qibYFaeYzHTHZvJqjkkfoDkalZtVaNMR/assets/static/optimized/rev-322d5f6/blog/wp-content/uploads/2021/04/Speedway-vinyl.jpeg"
-              text="Here is my collection of rare vinyl records."
+              text="Come see my collection of rare vinyl records."
               label="Vinyl Records"
               path="/vinylRecords"
-              className="feed-li"
+            />
+            <CardItem
+              src="https://www.motortrend.com/uploads/sites/11/2013/10/1966-1977-Ford-Bronco-front-right-view.jpg"
+              text="Come see my collection of rare vintage cars."
+              label="Vintage Cars"
+              path="/vintageCars"
             />
           </ul>
         </div>
