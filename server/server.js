d4204411075a7b95aa741f5801d7eceb427a469b
Daniel Bonn <danieltbonn@gmail.com>
2023-11-18 14:14:57 -0600

    deleted unnecessary js and uncommented out the me path

--- server/server.js
+++ server/server.js
@@ -20,47 +20,6 @@ const server = new ApolloServer({
 });
 
 
-// const S3_BUCKET='collectors-connect-collections-bucket'
-// const AWS_ACCESS_KEY_ID='AKIAX4RHXPPRJGIUV3RX'
-// const AWS_SECRET_ACCESS_KEY='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'
-// const AWS_REGION='us-east-1'
-
-// AWS.config.update({
-//   accessKeyId: AWS_ACCESS_KEY_ID,
-//   secretAccessKey: AWS_SECRET_ACCESS_KEY,
-//   region: AWS_REGION,
-// });
-// const s3 = new AWS.S3({
-// //   params: { 
-// //     Bucket: S3_BUCKET,
-// // },
-// //   region: AWS_REGION,
-// });
-
-// const params = {
-//   Bucket: S3_BUCKET,
-//   Key: 'Example.jpg'
-// }
-
-// app.get('/images/:key', async (req, res) => {
-//   console.log("Hello")
-//   const imageData = await s3.getObject(params, function(err,data) {
-//     if (err) console.log(err, err.stack);
-//     else {
-//         console.log(data)
-//         console.log(data.Body)
-//         return data;
-//     };
-// }).promise();
-
-//   console.log(imageData)
-//   console.log('hmm')
-//   res.contentType('image/jpeg');
-//   res.setHeader('Content-Type', imageData.ContentType);
-//   res.send(imageData.Body);
-// });
-
-
 const startApolloServer = async () => {
   await server.start();
 
