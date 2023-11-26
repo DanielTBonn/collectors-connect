de37b8e108cff960e38957b2fde002d3079cc4bc
k8 <katesterchi@gmail.com>
2023-11-25 20:17:58 -0600

    added inline styling to match profile page

--- client/src/pages/SingleCollectionById.jsx
+++ client/src/pages/SingleCollectionById.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import { GET_SINGLE_COLLECTION } from "../utils/queries";
+import { GET_SINGLE_COLLECTION, GET_ME } from "../utils/queries";
 import ItemsComponent from "../components/ItemsComponent";
+import CollectionImageComponent from "../components/CollectionImageComponent";
 
 const SingleCollectionById = () => {
   const { collectionId } = useParams();
@@ -13,23 +15,25 @@ const SingleCollectionById = () => {
     variables: { collectionId },
   });
 
-  if (collectionLoading) return <p>Loading...</p>;
-  if (collectionError) return <p>Error: {collectionError.message}</p>;
+  const { loading: userLoading, data: userData } = useQuery(GET_ME);
 
-  if (!collectionData || !collectionData.singleCollection) {
-    return <p>No data found for collection {collectionId}</p>;
-  }
+  if (collectionLoading || userLoading) return <p>Loading...</p>;
+  if (collectionError) return <p>Error: {collectionError.message}</p>;
 
   const { singleCollection } = collectionData;
 
-  console.log(singleCollection);
+  const inlineStyles = {
+    margin: "10px",
+    padding: "15px",
+    fontSize: "16px",
+    
+  };
 
   return (
-    <div>
+    <div style={inlineStyles}>
       <h2>{singleCollection.name}</h2>
+      <CollectionImageComponent collection={singleCollection} />
       <p>{singleCollection.description}</p>
-
-      {/* Display the items for the collection */}
       <h3>Items:</h3>
       <ItemsComponent />
     </div>
@@ -37,3 +41,4 @@ const SingleCollectionById = () => {
 };
 
 export default SingleCollectionById;
+
